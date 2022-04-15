c9b698e1b87d025c1bd83ca0e4e0108b01761f99
joohe71 <joohe71@gmail.com>
2022-04-15 19:40:15 +0900

    Fix keyError in RegisterList.js

--- register-app/src/registerform/RegisterList.js
+++ register-app/src/registerform/RegisterList.js
@@ -3,7 +3,7 @@ const RegisterList = ({userList}) => {
   return (
     <div>
       <h4>실시간 회원가입 유저 리스트</h4>
-      {userList.map((item) => <p>{item.id} 님이 회원가입 하셨습니다.</p>)}
+      {userList.map((item) => <p key={item._id}>{item.id} 님이 회원가입 하셨습니다.</p>)}
     </div>
   )
 }
