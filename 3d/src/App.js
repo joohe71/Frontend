b6c7d88f5ad2f6a78e795486a28a73e5438430ca
joohe71 <joohe71@gmail.com>
2022-08-22 21:51:31 +0900

    add: 3d 정육면체 예제

--- 3d/src/App.js
+++ 3d/src/App.js
@@ -1,5 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <mesh position={[0, 0.5, 0]}>
+          <boxBufferGeometry attatch="geometry" />
+          <meshLambertMaterial attatch="material" color="orange" />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
